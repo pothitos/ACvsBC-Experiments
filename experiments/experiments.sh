cfc72ba4e3efca09767b6efc0bab10afa7943cfb
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-10-21 17:59:14 +0300

    Wrap long lines

--- experiments/experiments.sh
+++ experiments/experiments.sh
@@ -9,16 +9,20 @@ for PARAM in $NQUEENS_RANGE
 do
     time -o AC_Time.txt -f "%e" ./nqueens.AC $PARAM > /dev/null
     time -o BC_Time.txt -f "%e" ./nqueens.BC $PARAM | tail -1 > n_d_e.txt
-    echo "NQueens\t$PARAM\t$(cat n_d_e.txt)\t$(cat AC_Time.txt)\t$(cat BC_Time.txt)"
+    echo "NQueens\t$PARAM\t$(cat n_d_e.txt)\t$(cat AC_Time.txt)\t$(cat
+          BC_Time.txt)"
 done
 echo
 echo
 CREW_SCHEDULING_RANGE=OR-Library/*
 for PARAM in $CREW_SCHEDULING_RANGE
 do
-    time -o AC_Time.txt -f "%e" crew_scheduling/crew_scheduling.AC $PARAM > /dev/null
-    time -o BC_Time.txt -f "%e" crew_scheduling/crew_scheduling.BC $PARAM | tail -1 > n_d_e.txt
-    echo "CrewSch\t$(basename $PARAM .txt)\t$(cat n_d_e.txt)\t$(cat AC_Time.txt)\t$(cat BC_Time.txt)"
+    time -o AC_Time.txt -f "%e" crew_scheduling/crew_scheduling.AC $PARAM > \
+        /dev/null
+    time -o BC_Time.txt -f "%e" crew_scheduling/crew_scheduling.BC $PARAM |
+        tail -1 > n_d_e.txt
+    echo "CrewSch\t$(basename $PARAM .txt)\t$(cat n_d_e.txt)\t$(cat
+          AC_Time.txt)\t$(cat BC_Time.txt)"
 done
 echo
 echo
@@ -36,6 +40,7 @@ for PARAM in $MAGIC_SQUARE_RANGE
 do
     time -o AC_Time.txt -f "%e" ./magic_square.AC $PARAM > /dev/null
     time -o BC_Time.txt -f "%e" ./magic_square.BC $PARAM | tail -1 > n_d_e.txt
-    echo "MagicSq\t$PARAM\t$(cat n_d_e.txt)\t$(cat AC_Time.txt)\t$(cat BC_Time.txt)"
+    echo "MagicSq\t$PARAM\t$(cat n_d_e.txt)\t$(cat AC_Time.txt)\t$(cat
+          BC_Time.txt)"
 done
 rm n_d_e.txt AC_Time.txt BC_Time.txt
