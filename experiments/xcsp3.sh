6356bf81c946c997f20da4e846de0d8bd4474357
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-10-25 07:37:34 +0300

    Compile against both solvers

--- experiments/xcsp3.sh
+++ experiments/xcsp3.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 set -ev
+
 # Compile XCSP3 mini-parser
 cd ../naxos-bounds-consistency/apps/XCSP3/
 cmake .
 make -j naxos-xcsp3
+mv naxos-xcsp3 naxos-xcsp3.BC
+
+# Compile against Arc Consistency solver
+#TODO: Patch CMakeLists.txt with recipe for Naxos Arc Consistency
+cmake .
+make -j
+mv naxos-xcsp3 naxos-xcsp3.AC
