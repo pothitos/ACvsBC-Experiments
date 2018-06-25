e3540e600a7d3534bd6aaff35c5a928fc99257dc
Nikolaos Pothitos <pothitos@di.uoa.gr>
2018-06-25 04:55:18 +0300

    Gather together tests

--- XCSP3/verification.sh
+++ XCSP3/verification.sh
@@ -20,6 +20,14 @@ make -j naxos-xcsp3
 if [ "$CONTINUOUS_INTEGRATION" = "true" ]
 then
     ctest -V
+    mv naxos-xcsp3 naxos-xcsp3.AC
+    cd ../../../XCSP3/
+    # Perform experiments for a limited time frame
+    sed -i.bak 's/40m/1m/' experiments.sh
+    ./experiments.sh verification/CheckerSlowInstances.txt
+    # Restore the original script
+    mv experiments.sh.bak experiments.sh
+    cd -
 fi
 
 # Forbid unimplemented propagators
@@ -29,13 +37,3 @@ cd -
 cmake .
 make -j naxos-xcsp3
 mv naxos-xcsp3 naxos-xcsp3.AC
-
-if [ "$CONTINUOUS_INTEGRATION" = "true" ]
-then
-    cd ../../../XCSP3/
-    # Perform experiments for a limited time frame
-    sed -i.bak 's/40m/1m/' experiments.sh
-    ./experiments.sh verification/CheckerSlowInstances.txt
-    # Restore the original script
-    mv experiments.sh.bak experiments.sh
-fi
